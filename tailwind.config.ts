fb134262fc43a7900ff0c2306073729e80946b10
ssuyeong <107804563+soo0297@users.noreply.github.com>
2025-03-16 00:31:38 +0900

    tailwindCSS 반응형 초기설정

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Tailwind가 적용될 파일 경로
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: "320px", // 모바일 (기본값)
+        md: "420px", // 넓은 모바일
+        lg: "768px", // 태블릿
+        xl: "1024px", // 데스크톱
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
